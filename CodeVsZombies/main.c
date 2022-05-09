0cf81b37f2d8bb0ed5537f7f096e1c26d1082fc2
ChoZeur <chozeur@protonmail.com>
2022-05-09 04:08:47 +0200

    +

--- CodeVsZombies/main.c
+++ CodeVsZombies/main.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/19 03:42:27 by flcarval          #+#    #+#             */
-/*   Updated: 2022/04/19 04:27:25 by flcarval         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   main.c											 :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: flcarval <flcarval@student.42.fr>		  +#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2022/04/19 03:42:27 by flcarval		  #+#	#+#			 */
+/*   Updated: 2022/05/09 02:16:49 by flcarval		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #include <stdlib.h>
@@ -15,47 +15,62 @@
 #include <string.h>
 #include <stdbool.h>
 
-typedef struct s_zombie{
-	int	id;
+# define CHECK fprintf(stderr, "check\n");
+
+
+typedef struct s_vector{
 	int	x;
 	int	y;
-	int	xnext;
-	int	ynext;
+}	t_vector;
+
+typedef struct s_zombie{
+	int			id;
+	t_vector	pos;
+	t_vector	next_pos;
 }	t_zombie;
 
 typedef struct s_human{
-	int	id;
-	int	x;
-	int	y;
+	int			id;
+	t_vector	pos;
+	int			fear;
 }	t_human;
 
 typedef struct s_data{
 	t_zombie	Zombies[2048];
 	t_human		Humans[2048];
+	int			zombie_count;
+	int			human_count;
+	int			round;
+	t_vector	last_move;
 }	t_data;
 
-static void	loop_get_humans(int human_count, t_data *data);
-static void	loop_get_zombies(int zombie_count, t_data *data);
+static void		loop_get_humans(int human_count, t_data *data);
+static void		loop_get_zombies(int zombie_count, t_data *data);
+static t_zombie	*id_to_zombie(int id, t_zombie *Zombies, int zombie_count);
+static int		fear_indice(t_human human, t_zombie *Zombies, int zombie_count);
+static double	distance(t_vector a, t_vector b);
+static void		set_fears(t_human *Humans, t_zombie *Zombies, int human_count, int zombie_count);
+static void		move(t_data *data);
 
 int main()
 {
 	t_data	data;
 	int	x;
 	int	y;
-	int	human_count;
-	int	zombie_count;
 
+	data.round = 0;
+	data.last_move.x = 0;
+	data.last_move.y = 0;
 	while (1)
 	{
+		data.round++;
 		scanf("%d%d", &x, &y);
-		scanf("%d", &human_count);
-		loop_get_humans(human_count, &data);
-		scanf("%d", &zombie_count);
-		loop_get_zombies(zombie_count, &data);
-
-		////////////////////////////////////////////////
-		printf("%d %d\n", data.Zombies[0].x, data.Zombies[0].y); // Your destination coordinates
-		////////////////////////////////////////////////
+		scanf("%d", &data.human_count);
+		loop_get_humans(data.human_count, &data);
+		scanf("%d", &data.zombie_count);
+		loop_get_zombies(data.zombie_count, &data);
+		set_fears(data.Humans, data.Zombies, data.human_count, data.zombie_count);
+		move(&data);
 	}
 	return (0);
 }
@@ -69,8 +84,9 @@ static void	loop_get_humans(int human_count, t_data *data)
 	for (int i = 0; i < human_count; i++) {
 		scanf("%d%d%d", &human_id, &human_x, &human_y);
 		data->Humans[i].id = human_id;
-		data->Humans[i].x = human_x;
-		data->Humans[i].y = human_y;
+		fprintf(stderr, "[loop_get_humans] Humans[%d].id = %d\n", i, data->Humans[i].id);
+		data->Humans[i].pos.x = human_x;
+		data->Humans[i].pos.y = human_y;
 	}
 }
 
@@ -85,9 +101,101 @@ static void	loop_get_zombies(int zombie_count, t_data *data)
 	for (int i = 0; i < zombie_count; i++) {
 		scanf("%d%d%d%d%d", &zombie_id, &zombie_x, &zombie_y, &zombie_xnext, &zombie_ynext);
 		data->Zombies[i].id = zombie_id;
-		data->Zombies[i].x = zombie_x;
-		data->Zombies[i].y = zombie_y;
-		data->Zombies[i].xnext = zombie_xnext;
-		data->Zombies[i].ynext = zombie_ynext;
+		data->Zombies[i].pos.x = zombie_x;
+		data->Zombies[i].pos.y = zombie_y;
+		data->Zombies[i].next_pos.x = zombie_xnext;
+		data->Zombies[i].next_pos.y = zombie_ynext;
 	}
+}
+
+static int	fear_indice(t_human human, t_zombie *Zombies, int zombie_count)
+{
+	int	res;
+	int	i;
+
+	res = 0;
+	i = 0;
+	while (i < zombie_count)
+	{
+		res += distance(Zombies[i].next_pos, human.pos);
+		i++;
+	}
+	res /= zombie_count;
+	fprintf(stderr, "[fear_indice] fear = %d\n", res);
+	return (res * (-1));
+}
+
+static t_zombie	*id_to_zombie(int id, t_zombie *Zombies, int zombie_count)
+{
+	int	i;
+
+	i = 0;
+	while (i < zombie_count)
+	{
+		fprintf(stderr, "id = %d zombie[%d].id = %d\n", id, i, Zombies[i].id);
+		if (Zombies[i].id == id)
+			return (&Zombies[i]);
+		i++;
+	}
+	fprintf(stderr, "[id _to_zombie] : error\n");
+	return (NULL);
+}
+
+static double	distance(t_vector a, t_vector b)
+{
+	return (sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2) * 1.0));
+}
+
+static void		set_fears(t_human *Humans, t_zombie *Zombies, int human_count, int zombie_count)
+{
+	int	i;
+
+	i = 0;
+	while (i < human_count)
+	{
+		Humans[i].fear = fear_indice(Humans[i], Zombies, zombie_count);
+		i++;
+	}
+}
+
+static void		move(t_data *data)
+{
+	// if (data->round < 16)
+	// 		printf("%d %d\n", data->Humans[0].pos.x, data->Humans[0].pos.y);
+	// else
+	// 		printf("%d %d\n", data->Zombies[0].pos.x, data->Zombies[0].pos.y); // Your destination coordinates
+
+	fprintf(stderr, "in [move]\n");
+	int			min_fear;
+	int			i;
+	int			id;
+	t_vector	move;
+	static int			keep = 0;
+
+	min_fear = 0;
+	i = 0;
+	while (i < data->human_count)
+	{
+		CHECK
+		fprintf(stderr, "i = %d human_count = %d\n", i, data->human_count);
+		if (data->Humans[i].fear < min_fear)
+		{
+			min_fear = data->Humans[i].fear;
+			id = data->Humans[i].id;
+			fprintf(stderr, "[move] id = %d\n", data->Humans[i].id);
+		}
+		i++;
+	}
+	move = data->Humans[id].pos;
+	fprintf(stderr, "[move] min_fear = %d\n", min_fear);
+	else if (data->last_move.x && data->last_move.y)
+	{
+		if (keep > 3)
+			keep = 0;
+		else
+			move = data->last_move;
+	}
+	printf("%d %d\n", move.x, move.y);
+	keep++;
+	data->last_move = move;
 }
--- ThereIsNoSpoon/ep01.c
+++ ThereIsNoSpoon/ep01.c
@@ -9,136 +9,154 @@
  **/
 
 typedef struct s_node{
-    int x1;
-    int y1;
-    int x2;
-    int y2;
-    int x3;
-    int y3;
-    int last;
+	int	x1;
+	int	y1;
+	int	x2;
+	int	y2;
+	int	x3;
+	int	y3;
+	int	last;
 }   t_node;
 
-static t_node *set_node_nodes(char **strnodes, int height, int width);
-static int isnode(int x1, int y1, t_node *nodes);
-static void set_neighborhood(t_node *nodes);
-static void set_no_neighboor(t_node node);
-static void print_nodes(t_node *nodes);
+static t_node	*set_node_nodes(char **strnodes, int height, int width);
+static int	isnode(int x1, int y1, t_node *nodes);
+static void	set_neighborhood(t_node *nodes, int width);
+static void	set_no_neighboor_right(t_node *node, int i);
+static void	set_no_neighboor_below(t_node *node, int i);
+static void	print_nodes(t_node *nodes);
 
 int main()
 {
-    // the number of cells on the X axis
-    int width;
-    scanf("%d", &width);
-    // the number of cells on the Y axis
-    int height;
-    scanf("%d", &height); fgetc(stdin);
-    char **strnodes = malloc(sizeof(char *) * height);
-    if (!strnodes)
-        return (-1);
-    for (int i = 0; i < height; i++) {
-        // width characters, each either 0 or .
-        char line[32];
-        scanf("%[^\n]", line); fgetc(stdin);
-        strnodes[i] = line;
-        fprintf(stderr, "%s\n", strnodes[i]);
-    }
-    t_node *nodes = set_node_nodes(strnodes, height, width);
-    set_neighborhood(nodes);
-    print_nodes(nodes);
-    return 0;
+	// the number of cells on the X axis
+	int	width;
+	scanf("%d", &width);
+	// the number of cells on the Y axis
+	int	height;
+	scanf("%d", &height); fgetc(stdin);
+	char	**strnodes = malloc(sizeof(char *) * height);
+	if (!strnodes)
+		return (-1);
+	for (int i = 0; i < height; i++) {
+		// width characters, each either 0 or .
+		char	line[32];
+		scanf("%[^\n]", line); fgetc(stdin);
+		strnodes[i] = strdup(line);
+		fprintf(stderr, "%s\n", strnodes[i]);
+	}
+	t_node	*nodes = set_node_nodes(strnodes, height, width);
+	set_neighborhood(nodes, width);
+	print_nodes(nodes);
+	return 0;
 }
 
-static t_node *set_node_nodes(char **strnodes, int height, int width)
+static t_node	*set_node_nodes(char **strnodes, int height, int width)
 {
-    int i;
-    int j;
-    t_node *nodes;
+	int		i;
+	int		j;
+	int		k;
+	t_node	*nodes;
 
-    if (!(nodes = malloc(sizeof(t_node) * ((height * width) + 1))))
-        return (NULL);
-    i = 0;
-    while (i < height)
-    {
-        j = 0;
-        while (strnodes[i][j])
-        {
-            if (strnodes[i][j] == '0')
-            {
-                nodes[i].x1 = j;
-                nodes[i].y1 = i;
-                nodes[i].last = 0;
-            }
-            j++;
-        }
-        i++;
-    }
-    nodes[i].last = 1;
-    return (nodes);
+	if (!(nodes = malloc(sizeof(t_node) * ((height * width) + 1))))
+		return (NULL);
+	i = 0;
+	k = 0;
+	while (i < height)
+	{
+		j = 0;
+		while (j < width)
+		{
+			fprintf(stderr, "%c", strnodes[i][j]);
+			if (strnodes[i][j] == '0')
+			{
+				nodes[k].x1 = j;
+				nodes[k].y1 = i;
+				nodes[k].last = 0;
+				set_no_neighboor_right(&nodes[k], -2);
+				set_no_neighboor_below(&nodes[k], -2);
+				k++;
+			}
+			j++;
+		}
+		fprintf(stderr, "\n");
+		i++;
+	}
+	nodes[k].last = 1;
+	return (nodes);
 }
 
-static int isnode(int x1, int y1, t_node *nodes)
+static int	isnode(int x1, int y1, t_node *nodes)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (!nodes[i].last)
-    {
-        if (nodes[i].x1 == x1)
-            if (nodes[i].y1 == y1)
-                return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (!nodes[i].last)
+	{
+		if (nodes[i].x1 == x1)
+			if (nodes[i].y1 == y1)
+				return (1);
+		i++;
+	}
+	return (0);
 }
 
-static void set_neighborhood(t_node *nodes)
+static void	set_neighborhood(t_node *nodes, int width)
 {
-    int i;
+	int	i;
+	int	j;
 
-    i = 0;
-    while (!nodes[i].last)
-    {
-        set_no_neighboor(nodes[i]);
-        if (isnode(nodes[i].x1 + 1, nodes[i].y1, nodes))
-        {
-            nodes[i].x2 = nodes[i].x1 + 1;
-            nodes[i].y2 = nodes[i].y1;
-        }
-        else
-        {
-            nodes[i].x2 = -1;
-            nodes[i].y2 = -1;
-        }
-        if (isnode(nodes[i].x1, nodes[i].y1 + 1, nodes))
-        {
-            nodes[i].x3 = nodes[i].x1;
-            nodes[i].y3 = nodes[i].y1 + 1;
-        }
-        else
-        {
-            nodes[i].x3 = -1;
-            nodes[i].y3 = -1;
-        }
-        i++;
-    }
+	i = 0;
+	while (!nodes[i].last)
+	{
+		j = 1;
+		while (j < width || j == 1)
+		{
+			if (isnode(nodes[i].x1 + j, nodes[i].y1, nodes))
+			{
+				if (nodes[i].x2 == -2 && nodes[i].y2 == -2)
+				{
+					nodes[i].x2 = nodes[i].x1 + j;
+					nodes[i].y2 = nodes[i].y1;
+				}
+			}
+			else if (nodes[i].x2 == -2 && nodes[i].y2 == -2 && j >= width - 1)
+				set_no_neighboor_right(&nodes[i], -1);
+			if (isnode(nodes[i].x1, nodes[i].y1 + j, nodes))
+			{
+				if (nodes[i].x3 == -2 && nodes[i].y3 == -2)
+				{
+					nodes[i].x3 = nodes[i].x1;
+					nodes[i].y3 = nodes[i].y1 + j;
+				}
+			}
+			else if (nodes[i].x3 == -2 && nodes[i].y3 == -2 && j >= width - 1)
+				set_no_neighboor_below(&nodes[i], -1);
+			j++;
+		}
+		i++;
+	}
 }
 
-static void set_no_neighboor(t_node node)
+static void	set_no_neighboor_right(t_node *node, int i)
 {
-    node.x2 = -1;
-    node.y2 = -1;
-    node.x3 = -1;
-    node.y3 = -1;
+	node->x2 = i;
+	node->y2 = i;
 }
 
-static void print_nodes(t_node *nodes)
+static void	set_no_neighboor_below(t_node *node, int i)
 {
-    int i;
+	node->x3 = i;
+	node->y3 = i;
+}
 
-    i = 0;
-    while (!nodes[i].last)
-    {
-        fprintf(stderr, "%d %d %d %d %d %d\n", nodes[i].x1, nodes[i].y1, nodes[i].x2, nodes[i].y2, nodes[i].x3, nodes[i].y3);
-        i++;
-    }
+static void	print_nodes(t_node *nodes)
+{
+	int	i;
+
+	i = 0;
+	while (!nodes[i].last)
+	{
+		printf("%d %d %d %d %d %d\n", nodes[i].x1, nodes[i].y1, \
+			nodes[i].x2, nodes[i].y2, nodes[i].x3, nodes[i].y3);
+		i++;
+	}
 }
