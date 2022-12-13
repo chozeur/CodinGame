4011f5195b3892c6c17e87fbd47905987af35e4b
ChoZeur <chozeur@protonmail.com>
2022-12-13 02:58:33 +0100

    fall22 wip

--- challenges/fallChallenge2022/fallChallenge2022.cpp
+++ challenges/fallChallenge2022/fallChallenge2022.cpp
@@ -29,64 +29,96 @@ public:
 	void	set_canbuild(bool canbuild){this->_canBuild = canbuild;}
 	void	set_canspawn(bool canspawn){this->_canSpawn = canspawn;}
 	void	set_inRangeOfRecycler(bool inRangeOfRecycler){this->_inRangeOfRecycler = inRangeOfRecycler;}
-	int		get_x(void){return this->_x;}
-	int		get_y(void){return this->_y;}
-	int		get_scrapAmount(void){return this->_scrapAmount;}
-	int		get_owner(void){return this->_owner;}
-	int		get_units(void){return this->_units;}
-	bool	get_recycler(void){return this->_recycler;}
-	bool	get_canbuild(void){return this->_canBuild;}
-	bool	get_canspawn(void){return this->_canSpawn;}
-	bool	get_inRangeOfRecycler(void){return this->_inRangeOfRecycler;}
+	int		get_x(void) const {return (this->_x);}
+	int		get_y(void) const {return (this->_y);}
+	int		get_scrapAmount(void) const {return (this->_scrapAmount);}
+	int		get_owner(void) const {return (this->_owner);}
+	int		get_units(void) const {return (this->_units);}
+	bool	get_recycler(void) const {return (this->_recycler);}
+	bool	get_canbuild(void) const {return (this->_canBuild);}
+	bool	get_canspawn(void) const {return (this->_canSpawn);}
+	bool	get_inRangeOfRecycler(void) const {return (this->_inRangeOfRecycler);}
+};
+
+
+/* MAP */
+class Map {
+private:
+	Position	**_mapArray;
+	int const	_width;
+	int const	_height;
+public:
+	Map(int width, int height):_width(width),_height(height){
+		this->_mapArray = new Position*[height];
+		for (int i = 0; i < height; ++i)
+			this->_mapArray[i] = new Position[width];
+		return ;
+	}
+	~Map(void){
+		for(int i = 0; i < this->_height; ++i)
+			delete[] this->_mapArray[i];
+		delete[] this->_mapArray;
+	}
+	void	set_mapArray(void){
+		for (int i = 0; i < this->_height; ++i){
+			for (int j = 0; j < this->_width; ++j){
+				int scrapAmount;
+				int owner; // 1 = me, 0 = foe, -1 = neutral
+				int units;
+				int recycler;
+				int canBuild;
+				int canSpawn;
+				int inRangeOfRecycler;
+				std::cin >> scrapAmount >> owner >> units >> recycler >> canBuild >> canSpawn >> inRangeOfRecycler; std::cin.ignore();
+				this->_mapArray[i][j].set_x(j);
+				this->_mapArray[i][j].set_y(i);
+				this->_mapArray[i][j].set_scrapAmount(scrapAmount);
+				this->_mapArray[i][j].set_owner(owner);
+				this->_mapArray[i][j].set_units(units);
+				this->_mapArray[i][j].set_recycler(recycler);
+				this->_mapArray[i][j].set_canbuild(canBuild);
+				this->_mapArray[i][j].set_canspawn(canSpawn);
+				this->_mapArray[i][j].set_inRangeOfRecycler(inRangeOfRecycler);
+			}
+		}
+		return ;
+	}
 };
 
 /* PLAYER */
 class Player {
 private:
-	int	_matter;
+	int			_matter;
+	Map const	&_map;
 public:
-	Player(void):_matter(0){}
+	Player(Map &map):_matter(0), _map(map){}
 	~Player(void){}
+	void	set_matter(int matter){this->_matter = matter;}
+	int		get_matter(void) const {return (this->_matter);}
+	void	move(void) const {
+		/* ADD HERE */
+	}
+	void	build(void) const {
+		/* ADD HERE */
+	}
+	void	spawn(void) const {
+		/* ADD HERE */
+	}
 };
 
 /* FUNCTIONS */
-void	setMap(Position **Map, int height, int width){
-for (int i = 0; i < height; i++){
-	for (int j = 0; j < width; j++){
-		int scrapAmount;
-		int owner; // 1 = me, 0 = foe, -1 = neutral
-		int units;
-		int recycler;
-		int canBuild;
-		int canSpawn;
-		int inRangeOfRecycler;
-		std::cin >> scrapAmount >> owner >> units >> recycler >> canBuild >> canSpawn >> inRangeOfRecycler; std::cin.ignore();
-		Map[i][j].set_x(j);
-		Map[i][j].set_y(i);
-		Map[i][j].set_scrapAmount(scrapAmount);
-		Map[i][j].set_owner(owner);
-		Map[i][j].set_units(units);
-		Map[i][j].set_recycler(recycler);
-		Map[i][j].set_canbuild(canBuild);
-		Map[i][j].set_canspawn(canSpawn);
-		Map[i][j].set_inRangeOfRecycler(inRangeOfRecycler);
-	}
-}
-	return ;
-}
 
 
-int main(){
+/* MAIN */
+int main(void){
 	int width;
 	int height;
 	std::cin >> width >> height; std::cin.ignore();
 
-	Position	**Map = new Position*[height];
-	for (int i = 0; i < height; ++i)
-		Map[i] = new Position[width];
+	Map	cmap(width, height);
 
-	Player	myself;
-	Player	opponent;
+	Player	myself(cmap);
+	// Player	opponent(cmap);
 
 	unsigned int	round = 0;
 	while (++round){
@@ -95,12 +127,14 @@ int main(){
 		int opp_matter;
 		std::cin >> my_matter >> opp_matter; std::cin.ignore();
 
-		setMap(Map, height, width);
-	}
+		cmap.set_mapArray();
 
-	for(int i = 0; i < height; ++i)
-		delete[] Map[i];
-	delete[] Map;
+		myself.move();
+		myself.build();
+		myself.spawn();
+
+		std::cout << "WAIT" << std::endl;
+	}
 
 	return (0);
 }
