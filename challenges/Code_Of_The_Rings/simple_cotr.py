61ca0e00b9023bc67e0996b7352f46a5c3f89fa6
ChoZeur <chozeur@protonmail.com>
2023-03-31 01:40:23 +0200

    cotr: need opti

--- challenges/Code_Of_The_Rings/simple_cotr.py
+++ challenges/Code_Of_The_Rings/simple_cotr.py
@@ -59,11 +59,11 @@ def costOfAll(forest, bpos, letter):
 
     return mapf
 
-def lowestCost(costs :dict, tmu):
+def lowestCost(costs :dict):
     m = 9999
     res = 0
     for i in costs.keys():
-        if costs[i] < m and i not in tmu:
+        if costs[i] < m:
             m = costs[i]
             res = i
     return res
@@ -73,12 +73,10 @@ def move(spell, forest, bpos, movepos, letter):
     if movepos > bpos:
         for i in range(movepos - bpos):
             spell += '>'
-            print(end='>')
             bpos += 1
     elif bpos > movepos:
         for i in range(abs(movepos - bpos)):
             spell += '<'
-            print(end='<')
             bpos -= 1
 
     print(f'forest[{movepos}] = "{forest[movepos]}"', file=sys.stderr)
@@ -87,51 +85,17 @@ def move(spell, forest, bpos, movepos, letter):
     if m == 'p':
         while forest[movepos] != letter:
             spell += '+'
-            print(end='+')
             forest[movepos] = plus(forest[movepos])
     elif m == 'm':
         while forest[movepos] != letter:
             spell += '-'
-            print(end='-')
             forest[movepos] = minus(forest[movepos])
 
-    print(end='.')
     return spell + '.', forest, bpos
 
-def tenMostUsed(magic_phrase):
-    char_counts = {}
-    for char in magic_phrase:
-        if char in char_counts and char != ' ':
-            char_counts[char] += 1
-        else:
-            if char != ' ':
-                char_counts[char] = 1
 
-    sorted_counts = sorted(char_counts.items(), key=lambda x: x[1], reverse=True)
-    most_found = [pair[0] for pair in sorted_counts][:10]
-
-    return most_found
-
-def findClosestFreeTier(forest, bpos):
-    left = right = -1
-    for i in range(bpos):
-        if i % 3 == 0:
-            if forest[i] == ' ':
-                left = i
-    for i in range(bpos,30):
-        if i % 3 == 0:
-            if forest[i] == ' ':
-                right = i
-                break
-
-    if min(left,right) != -1:
-        return min(left,right)
-    else:
-        return max(left,right)
 
 magic_phrase = input()
-tmu = tenMostUsed(magic_phrase)
-print(f'tmu = {tmu}', file=sys.stderr)
 
 spell = ''
 
@@ -141,19 +105,9 @@ bpos = 14
 
 i = 0
 while i != len(magic_phrase):
-    char = magic_phrase[i]
-    if char in forest:
-        print(f'{char} is in forest', file=sys.stderr)
-        movepos = forest.index(char)
-    elif char in tmu:
-        print(f'{char} is in tmu and need to be set', file=sys.stderr)
-        cft = findClosestFreeTier(forest, bpos)
-        movepos = cft
-    else:
-        print(f'{char} is nowhere', file=sys.stderr)
-        costs = costOfAll(forest, bpos, char)
-        movepos = lowestCost(costs, tmu)
-    spell, forest, bpos = move(spell, forest, bpos, movepos, char)
+    costs = costOfAll(forest, bpos, magic_phrase[i])
+    movepos = lowestCost(costs)
+    spell, forest, bpos = move(spell, forest, bpos, movepos, magic_phrase[i])
     print(f'spell = {spell}\n\nforest = {forest}\n\nbpos = {bpos}\n\nmovepos = {movepos}\n\n', file=sys.stderr)
     i+=1
-print()
+print(spell)
